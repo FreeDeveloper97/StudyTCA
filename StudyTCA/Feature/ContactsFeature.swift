3a47f3ed15c78986db63cc875a539b3e0eb33e55
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-08-28 22:55:18 +0900

    Feat: Testing the delete contact flow

--- StudyTCA/Feature/ContactsFeature.swift
+++ StudyTCA/Feature/ContactsFeature.swift
@@ -58,15 +58,7 @@ struct ContactsFeature: Reducer {
                 return .none
                 
             case let .deleteButtonTapped(id: id):
-                state.destination = .alert(
-                    AlertState {
-                        TextState("Are you sure?")
-                    } actions: {
-                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
-                            TextState("Delete")
-                        }
-                    }
-                )
+                state.destination = .alert(.deleteConfirmation(id: id))
                 return .none
             }
         }
@@ -93,6 +85,18 @@ extension ContactsFeature {
         var body: some ReducerOf<Self> {
             Scope(state: /State.addContact, action: /Action.addContact) {
                 AddContactFeature()
+            }
+        }
+    }
+}
+
+extension AlertState where Action == ContactsFeature.Action.Alert {
+    static func deleteConfirmation(id: UUID) -> Self {
+        Self {
+            TextState("Are you sure?")
+        } actions: {
+            ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                TextState("Delete")
             }
         }
     }
