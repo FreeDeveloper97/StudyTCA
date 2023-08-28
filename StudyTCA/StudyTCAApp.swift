aacec60032e898b62c090bacef9e63a88bfa0422
FreeDeveloper97 <freedeveloper97@gmail.com>
2023-08-28 12:02:29 +0900

    Feat: Create Project

---
+++ StudyTCA/StudyTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTCAApp.swift
+//  StudyTCA
+//
+//  Created by Kang Minsang on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct StudyTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
